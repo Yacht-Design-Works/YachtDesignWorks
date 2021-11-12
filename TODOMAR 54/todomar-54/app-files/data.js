69b32140dd2e74fd4fecadf5189f467d1b0debab
Yacht Design Works <31591598+YachtDesignWorks@users.noreply.github.com>
2021-11-11 20:01:50 -0500

    Todomar Exterior and interior

---
+++ TODOMAR 54/todomar-54/app-files/data.js
@@ -0,0 +1,657 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-view-1",
+      "name": "View 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6330562367926609,
+        "pitch": 0.33484361368981475,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.017201871558198,
+          "pitch": 0.2473153180304113,
+          "rotation": 3.141592653589793,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 1.411958887376839,
+          "pitch": 0.18142328454959866,
+          "rotation": 3.141592653589793,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 0.9748538755185177,
+          "pitch": 0.13230707610429882,
+          "rotation": 3.141592653589793,
+          "target": "5-transom"
+        },
+        {
+          "yaw": 0.27344304652801554,
+          "pitch": 0.012438298678537407,
+          "rotation": 2.356194490192345,
+          "target": "1-view-2"
+        },
+        {
+          "yaw": 1.159372324909068,
+          "pitch": 0.145623721321396,
+          "rotation": 3.141592653589793,
+          "target": "4-aft-lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-view-2",
+      "name": "View 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.687147319881456,
+        "pitch": 0.2760421017640784,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.159389891668198,
+          "pitch": 0.18119772503096776,
+          "rotation": 3.141592653589793,
+          "target": "5-transom"
+        },
+        {
+          "yaw": 1.3656365376933,
+          "pitch": 0.13039397144689957,
+          "rotation": 3.141592653589793,
+          "target": "4-aft-lounge"
+        },
+        {
+          "yaw": 1.7597377744812635,
+          "pitch": 0.00684900875789296,
+          "rotation": 9.42477796076938,
+          "target": "3-helm"
+        },
+        {
+          "yaw": 2.0482110072053628,
+          "pitch": 0.05281011298021454,
+          "rotation": 3.141592653589793,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 2.647489665398174,
+          "pitch": -0.08684825876356683,
+          "rotation": 3.9269908169872414,
+          "target": "0-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bow",
+      "name": "Bow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5176199151391625,
+        "pitch": 0.6125543203016228,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4299697377881717,
+          "pitch": -0.3328328746666358,
+          "rotation": 2.356194490192345,
+          "target": "0-view-1"
+        },
+        {
+          "yaw": -1.7298243467171392,
+          "pitch": 0.05834136314921068,
+          "rotation": 0,
+          "target": "3-helm"
+        },
+        {
+          "yaw": -2.0602300446265005,
+          "pitch": -0.18307184015884914,
+          "rotation": 2.356194490192345,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-helm",
+      "name": "Helm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.562334750996687,
+        "pitch": 0.5243805744582772,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3773112308008724,
+          "pitch": 0.11363836373571701,
+          "rotation": 0,
+          "target": "2-bow"
+        },
+        {
+          "yaw": -1.3556559071950431,
+          "pitch": 0.48188459346708257,
+          "rotation": 0,
+          "target": "4-aft-lounge"
+        },
+        {
+          "yaw": -1.5228897900348173,
+          "pitch": 0.20014826361911453,
+          "rotation": 0,
+          "target": "5-transom"
+        },
+        {
+          "yaw": 0.5914432383888517,
+          "pitch": 0.45153025671665326,
+          "rotation": 6.283185307179586,
+          "target": "6-hallway"
+        },
+        {
+          "yaw": 2.18591530067214,
+          "pitch": -0.1732161910070431,
+          "rotation": 2.356194490192345,
+          "target": "0-view-1"
+        },
+        {
+          "yaw": -2.11445154764597,
+          "pitch": -0.04420113604050613,
+          "rotation": 9.42477796076938,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-aft-lounge",
+      "name": "Aft Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.8739633822786512,
+        "pitch": 0.5439052375040134,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18263922496072205,
+          "pitch": 0.6252480769230342,
+          "rotation": 0,
+          "target": "5-transom"
+        },
+        {
+          "yaw": -2.9593556435653934,
+          "pitch": 0.0632241986081592,
+          "rotation": 0,
+          "target": "3-helm"
+        },
+        {
+          "yaw": -0.9098015360634282,
+          "pitch": -0.187876978797199,
+          "rotation": 10.210176124166829,
+          "target": "1-view-2"
+        },
+        {
+          "yaw": -2.5918807009208624,
+          "pitch": -0.03533030125849734,
+          "rotation": 10.995574287564278,
+          "target": "2-bow"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-transom",
+      "name": "Transom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.2933898610415948,
+        "pitch": 0.654608324556559,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8349002200259807,
+          "pitch": 0.5742131319443438,
+          "rotation": 0,
+          "target": "4-aft-lounge"
+        },
+        {
+          "yaw": -1.8084176989523826,
+          "pitch": 0.06180156067861198,
+          "rotation": 0,
+          "target": "3-helm"
+        },
+        {
+          "yaw": -1.500263099637559,
+          "pitch": -0.01695592291150838,
+          "rotation": 5.497787143782138,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 0.10269998033779437,
+          "pitch": -0.3840751987043163,
+          "rotation": 3.9269908169872414,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.580847578679883,
+        "pitch": 0.7048340916301505,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.078053875744903,
+          "pitch": 0.6200263117132412,
+          "rotation": 0,
+          "target": "7-fwd-berth"
+        },
+        {
+          "yaw": -0.24851831621822384,
+          "pitch": 0.4923243183003052,
+          "rotation": 0,
+          "target": "9-head--shower"
+        },
+        {
+          "yaw": 0.705210180340341,
+          "pitch": 0.7268940128414645,
+          "rotation": 0,
+          "target": "10-mid-berth"
+        },
+        {
+          "yaw": 1.4019587862356175,
+          "pitch": -0.2863561988404868,
+          "rotation": 0,
+          "target": "3-helm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-fwd-berth",
+      "name": "Fwd Berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6778955286164772,
+        "pitch": 0.697456317630067,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7574065496698017,
+          "pitch": 0.38988651342029,
+          "rotation": 3.141592653589793,
+          "target": "8-fwd-berth-on-bed"
+        },
+        {
+          "yaw": -1.3305255769025752,
+          "pitch": 0.8606811324909778,
+          "rotation": 3.141592653589793,
+          "target": "6-hallway"
+        },
+        {
+          "yaw": -1.58709854640108,
+          "pitch": 0.46202894794684823,
+          "rotation": 0,
+          "target": "10-mid-berth"
+        },
+        {
+          "yaw": -1.8568619457958988,
+          "pitch": 0.2730337797336837,
+          "rotation": 10.995574287564278,
+          "target": "9-head--shower"
+        },
+        {
+          "yaw": -1.2410078480305877,
+          "pitch": -0.14840875860263125,
+          "rotation": 0,
+          "target": "3-helm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-fwd-berth-on-bed",
+      "name": "Fwd Berth on Bed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.817791680175187,
+        "pitch": 0.32927601071527945,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9342985170397462,
+          "pitch": 0.2584188790970696,
+          "rotation": 0,
+          "target": "6-hallway"
+        },
+        {
+          "yaw": 2.008560229766612,
+          "pitch": -0.1757208746934822,
+          "rotation": 0,
+          "target": "3-helm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-head--shower",
+      "name": "Head & Shower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0730986623033125,
+        "pitch": 0.9001921904421053,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8596550271762062,
+          "pitch": 0.5638874989023286,
+          "rotation": 1.5707963267948966,
+          "target": "7-fwd-berth"
+        },
+        {
+          "yaw": -1.2872064223622157,
+          "pitch": 0.5916840089522903,
+          "rotation": 3.141592653589793,
+          "target": "6-hallway"
+        },
+        {
+          "yaw": -1.8436991456749148,
+          "pitch": 0.6298045849918381,
+          "rotation": 4.71238898038469,
+          "target": "10-mid-berth"
+        },
+        {
+          "yaw": -1.7106635282150169,
+          "pitch": -0.19518990774815492,
+          "rotation": 5.497787143782138,
+          "target": "3-helm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-mid-berth",
+      "name": "Mid Berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6636911462170438,
+        "pitch": 0.6507808216930719,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4656630578461574,
+          "pitch": 0.12003721211143414,
+          "rotation": 0,
+          "target": "7-fwd-berth"
+        },
+        {
+          "yaw": -1.7918793114641236,
+          "pitch": 0.20123932182359283,
+          "rotation": 3.141592653589793,
+          "target": "6-hallway"
+        },
+        {
+          "yaw": -1.3624315246326777,
+          "pitch": 0.2794210016017473,
+          "rotation": 1.5707963267948966,
+          "target": "9-head--shower"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "TODOMAR 54",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

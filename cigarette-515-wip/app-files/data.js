6eb6fbf8a5ef7cdbbb979dd9103079fb5a7d5a75
Yacht Design Works <31591598+YachtDesignWorks@users.noreply.github.com>
2021-12-09 11:14:13 -0500

    515 wip

---
+++ cigarette-515-wip/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-on-deck",
+      "name": "On Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.0148557559353844,
+        "pitch": 0.4744171162677233,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cigarette-515 WIP",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};

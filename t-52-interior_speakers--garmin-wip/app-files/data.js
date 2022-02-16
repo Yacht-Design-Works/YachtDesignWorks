16d6d83eec1cf359be8dedc23ddf56f5c4703e11
Yacht Design Works <31591598+YachtDesignWorks@users.noreply.github.com>
2022-02-16 17:07:02 -0500

    WIP

---
+++ t-52-interior_speakers--garmin-wip/app-files/data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-concept-a_location-1",
+      "name": "Concept A_Location 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.595346277662557,
+        "pitch": 0.15707111022500797,
+        "fov": 1.3646202047689777
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.702531804302133,
+          "pitch": -0.16302847164326195,
+          "rotation": 4.71238898038469,
+          "target": "2-concept-b_location-1"
+        },
+        {
+          "yaw": -1.3075048081686997,
+          "pitch": 0.6166393481255383,
+          "rotation": 0,
+          "target": "1-concept-a_location-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-concept-a_location-2",
+      "name": "Concept A_Location 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.9157842408808463,
+        "pitch": 0.3400095692869556,
+        "fov": 1.3646202047689777
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5844966990942204,
+          "pitch": 0.13484260916962576,
+          "rotation": 4.71238898038469,
+          "target": "3-concept-b_location-2"
+        },
+        {
+          "yaw": 0.7660392533441893,
+          "pitch": -0.01829717377458273,
+          "rotation": 0,
+          "target": "0-concept-a_location-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-concept-b_location-1",
+      "name": "Concept B_Location 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7150583516444176,
+        "pitch": 0.08858405428531846,
+        "fov": 1.3646202047689777
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6887869495586845,
+          "pitch": -0.3242125104882554,
+          "rotation": 4.71238898038469,
+          "target": "0-concept-a_location-1"
+        },
+        {
+          "yaw": -1.2027787444347382,
+          "pitch": 0.6078375580385185,
+          "rotation": 0,
+          "target": "3-concept-b_location-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-concept-b_location-2",
+      "name": "Concept B_Location 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.0736305048168777,
+        "pitch": 0.39100484954401793,
+        "fov": 1.3646202047689777
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5930429517016798,
+          "pitch": -0.012881842783283304,
+          "rotation": 4.71238898038469,
+          "target": "1-concept-a_location-2"
+        },
+        {
+          "yaw": 0.8157318245217091,
+          "pitch": -0.05439181737812859,
+          "rotation": 0,
+          "target": "2-concept-b_location-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "T-52 Interior_Speakers & Garmin WIP",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

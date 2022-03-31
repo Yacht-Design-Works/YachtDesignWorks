92bb0299c8b540e704d377dd1d2708c2777f8d31
Yacht Design Works <31591598+YachtDesignWorks@users.noreply.github.com>
2022-03-31 16:21:42 -0400

    515 update

---
+++ cigarette-515-wip-20220331/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_20211222--cigarette-515-master-model-working-tak_2022-03-31-16-11-56",
+      "name": "Panorama_20211222  Cigarette 515 Master Model Working Tak_2022-03-31-16-11-56",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5833697120991088,
+        "pitch": 0.5940054004657149,
+        "fov": 1.4175088064025105
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cigarette 515 WIP 20220331",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};

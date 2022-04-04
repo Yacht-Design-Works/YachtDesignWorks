824eb8bea88dbd5aba3ddafa1004499390144bc6
Yacht Design Works <31591598+YachtDesignWorks@users.noreply.github.com>
2022-04-04 15:02:10 -0400

    Cig 515 update

---
+++ 20220404--cigarette-515-design-development/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-20220404--cigarette-515-design-development",
+      "name": "20220404  Cigarette 515 Design Development",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.4860769903738031,
+        "pitch": 0.6253812216218115,
+        "fov": 1.4175088064025105
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20220404  Cigarette 515 Design Development",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

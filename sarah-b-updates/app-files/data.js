9c892ff9c4692341b0a1a4acd9cb7668bf1babfa
Yacht Design Works <tak@yachtdesignworks.com>
2023-08-07 11:59:38 -0400

    Sarah B

---
+++ sarah-b-updates/app-files/data.js
@@ -0,0 +1,845 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salon",
+      "name": "SALON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.566428337009155,
+        "pitch": 0.4055714446663998,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5360015324984122,
+          "pitch": 0.0942044578865815,
+          "rotation": 3.141592653589793,
+          "target": "2-helm"
+        },
+        {
+          "yaw": 0.36213021173989546,
+          "pitch": 0.21581130375145463,
+          "rotation": 3.141592653589793,
+          "target": "1-galley"
+        },
+        {
+          "yaw": 0.9273607343047097,
+          "pitch": 0.1484731270714903,
+          "rotation": 8.63937979737193,
+          "target": "3-lower-hallway-aft"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-galley",
+      "name": "GALLEY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5780032957242138,
+        "pitch": 0.2717200408787406,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.186424273782544,
+          "pitch": 0.27976472863233326,
+          "rotation": 2.356194490192345,
+          "target": "3-lower-hallway-aft"
+        },
+        {
+          "yaw": 1.919477737093425,
+          "pitch": 0.08874318504243028,
+          "rotation": 3.141592653589793,
+          "target": "2-helm"
+        },
+        {
+          "yaw": -2.7764319467548013,
+          "pitch": 0.19115021426008738,
+          "rotation": 3.141592653589793,
+          "target": "0-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-helm",
+      "name": "HELM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7421863148482757,
+        "pitch": 0.42960968453430937,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1398895280881298,
+          "pitch": 0.15241283481035772,
+          "rotation": 3.141592653589793,
+          "target": "0-salon"
+        },
+        {
+          "yaw": 1.5526868833213614,
+          "pitch": 0.15258083579879766,
+          "rotation": 3.141592653589793,
+          "target": "1-galley"
+        },
+        {
+          "yaw": 2.130154297211657,
+          "pitch": 0.17263973692337942,
+          "rotation": 3.9269908169872414,
+          "target": "3-lower-hallway-aft"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lower-hallway-aft",
+      "name": "LOWER HALLWAY AFT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.351550382726023,
+        "pitch": 0.21111373447453374,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0085496323105865,
+          "pitch": 0.19499210005704626,
+          "rotation": 0,
+          "target": "5-master-sartboard"
+        },
+        {
+          "yaw": 2.4478532387388423,
+          "pitch": 0.13167695417210545,
+          "rotation": 1.5707963267948966,
+          "target": "13-sartboard-cabin"
+        },
+        {
+          "yaw": 1.9192238597806162,
+          "pitch": 0.012832866932965814,
+          "rotation": 0,
+          "target": "15-vip-cabin"
+        },
+        {
+          "yaw": 1.924901446500412,
+          "pitch": 0.24307185484867766,
+          "rotation": 3.141592653589793,
+          "target": "8-lower-hallway-fwd"
+        },
+        {
+          "yaw": 1.8318261465993517,
+          "pitch": 0.06908376550674866,
+          "rotation": 4.71238898038469,
+          "target": "7-laundry-room"
+        },
+        {
+          "yaw": 0.5837640415175684,
+          "pitch": -0.3180563731113537,
+          "rotation": 5.497787143782138,
+          "target": "1-galley"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-master-port",
+      "name": "MASTER PORT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1320514851296135,
+        "pitch": 0.3894464321638047,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.852341570971184,
+          "pitch": 0.16071840461532005,
+          "rotation": 3.141592653589793,
+          "target": "5-master-sartboard"
+        },
+        {
+          "yaw": 2.502534951324976,
+          "pitch": 0.182055743958486,
+          "rotation": 5.497787143782138,
+          "target": "3-lower-hallway-aft"
+        },
+        {
+          "yaw": -1.0399643326037715,
+          "pitch": 0.09241271586296307,
+          "rotation": 0,
+          "target": "9-master-head"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-master-sartboard",
+      "name": "MASTER SARTBOARD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1566772951853377,
+        "pitch": 0.2596660345559574,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8845755609322037,
+          "pitch": 0.15739981619261378,
+          "rotation": 0.7853981633974483,
+          "target": "3-lower-hallway-aft"
+        },
+        {
+          "yaw": -0.06777808805725805,
+          "pitch": 0.14192016289404386,
+          "rotation": 3.141592653589793,
+          "target": "4-master-port"
+        },
+        {
+          "yaw": -2.1442188251267797,
+          "pitch": 0.3356858504582654,
+          "rotation": 0,
+          "target": "6-owners-cabin_walkin-closet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-owners-cabin_walkin-closet",
+      "name": "Owner's Cabin_Walkin-Closet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.39899471502745243,
+        "pitch": 0.3796063460477228,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.949538177320407,
+          "pitch": 0.2860307053524558,
+          "rotation": 0,
+          "target": "5-master-sartboard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-laundry-room",
+      "name": "LAUNDRY ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8740094203444375,
+        "pitch": 0.3636244457136826,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5491175876122654,
+          "pitch": 0.3314451180482276,
+          "rotation": 0,
+          "target": "8-lower-hallway-fwd"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-lower-hallway-fwd",
+      "name": "LOWER HALLWAY FWD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6392536212574225,
+        "pitch": 0.18826081112310966,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4015272119221862,
+          "pitch": 0.3891046878048243,
+          "rotation": 3.141592653589793,
+          "target": "3-lower-hallway-aft"
+        },
+        {
+          "yaw": 1.4046512903857096,
+          "pitch": 0.15255212022864129,
+          "rotation": 0,
+          "target": "5-master-sartboard"
+        },
+        {
+          "yaw": 2.4986933965206726,
+          "pitch": 0.3815130446934525,
+          "rotation": 0.7853981633974483,
+          "target": "7-laundry-room"
+        },
+        {
+          "yaw": 1.1591213012751158,
+          "pitch": 0.3299185031340883,
+          "rotation": 5.497787143782138,
+          "target": "14-shared-head"
+        },
+        {
+          "yaw": 1.5981083544043058,
+          "pitch": 0.17430757787119333,
+          "rotation": 0.7853981633974483,
+          "target": "12-port-cabin"
+        },
+        {
+          "yaw": -1.5931718868974176,
+          "pitch": 0.08248906516110921,
+          "rotation": 0,
+          "target": "15-vip-cabin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-master-head",
+      "name": "MASTER HEAD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6976360301884936,
+        "pitch": 0.38163383832016784,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.925711220794053,
+          "pitch": 0.1879399684550993,
+          "rotation": 12.566370614359176,
+          "target": "10-master-shower"
+        },
+        {
+          "yaw": -2.6343801341059248,
+          "pitch": 0.17969960457652334,
+          "rotation": 0,
+          "target": "11-master-toilet"
+        },
+        {
+          "yaw": -1.0049292624051773,
+          "pitch": 0.23583440178126658,
+          "rotation": 0,
+          "target": "4-master-port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-master-shower",
+      "name": "MASTER SHOWER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9296380683119745,
+        "pitch": 0.21095000271580844,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.642182413416407,
+          "pitch": 0.22333369488952748,
+          "rotation": 0,
+          "target": "9-master-head"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-master-toilet",
+      "name": "MASTER TOILET",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2389335766042144,
+        "pitch": 0.7175661606276158,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2064877674390093,
+          "pitch": 0.31253326993857655,
+          "rotation": 0,
+          "target": "9-master-head"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-port-cabin",
+      "name": "PORT CABIN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9888839919624459,
+        "pitch": 0.29512837505409273,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0301078876127967,
+          "pitch": 0.344652569340246,
+          "rotation": 12.566370614359176,
+          "target": "8-lower-hallway-fwd"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-sartboard-cabin",
+      "name": "SARTBOARD CABIN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.124632603182423,
+        "pitch": 0.2797414306244672,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9546639730685129,
+          "pitch": 0.2518306876066525,
+          "rotation": 0,
+          "target": "14-shared-head"
+        },
+        {
+          "yaw": -0.8039831425469401,
+          "pitch": 0.2323835005305206,
+          "rotation": 0,
+          "target": "3-lower-hallway-aft"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-shared-head",
+      "name": "SHARED HEAD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.8739339563622579,
+        "pitch": 0.3384701544958162,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3885570072153186,
+          "pitch": 0.2327451815191175,
+          "rotation": 0,
+          "target": "8-lower-hallway-fwd"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-vip-cabin",
+      "name": "VIP CABIN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5571858563832999,
+        "pitch": 0.21290393829388066,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08735623425912209,
+          "pitch": 0.15187303148246478,
+          "rotation": 0,
+          "target": "17-vip-shower"
+        },
+        {
+          "yaw": -2.9940773745098443,
+          "pitch": 0.11775841464010384,
+          "rotation": 0,
+          "target": "16-vip-head"
+        },
+        {
+          "yaw": -0.8738426061740725,
+          "pitch": 0.20767277847188126,
+          "rotation": 5.497787143782138,
+          "target": "8-lower-hallway-fwd"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-vip-head",
+      "name": "VIP HEAD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.489266295464887,
+        "pitch": 0.38072192950696504,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6728407918938508,
+          "pitch": 0.13677415008395144,
+          "rotation": 0,
+          "target": "15-vip-cabin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-vip-shower",
+      "name": "VIP SHOWER",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9356296376506794,
+        "pitch": 0.39088801630533254,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.549354940018242,
+          "pitch": 0.31969235449696143,
+          "rotation": 0,
+          "target": "15-vip-cabin"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Sarah B Updates",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

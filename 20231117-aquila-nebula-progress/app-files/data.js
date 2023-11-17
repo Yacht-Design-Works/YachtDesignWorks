3f35316bdfb57986cad30d5c591bddb418d5046b
Tak <65673253+Yacht-Design-Works@users.noreply.github.com>
2023-11-17 14:50:29 -0500

    Nebula A Progress

---
+++ 20231117-aquila-nebula-progress/app-files/data.js
@@ -0,0 +1,735 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-aft-deck---center",
+      "name": "Aft Deck - Center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5828617472229771,
+        "pitch": 0.21425419614630137,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8074300993551873,
+          "pitch": 0.655099302023098,
+          "rotation": 15.707963267948973,
+          "target": "5-aft-deck-"
+        },
+        {
+          "yaw": 0.5689437231922145,
+          "pitch": 0.8023354265137588,
+          "rotation": 3.141592653589793,
+          "target": "1-aft-decklower-step---port"
+        },
+        {
+          "yaw": 1.584817446089989,
+          "pitch": 0.16871101759634044,
+          "rotation": 3.141592653589793,
+          "target": "8-wetbar"
+        },
+        {
+          "yaw": 1.8862299579060284,
+          "pitch": 0.11416728012149235,
+          "rotation": 9.42477796076938,
+          "target": "10-midship---starboard"
+        },
+        {
+          "yaw": 1.3049607318521765,
+          "pitch": 0.1061674141292972,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-aft-decklower-step---port",
+      "name": "Aft Deck(Lower Step) - Port",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.9238860849970365,
+        "pitch": 0.4235450390508788,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8066975432502126,
+          "pitch": 0.5707146107778627,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-deck---center"
+        },
+        {
+          "yaw": 2.965677652068253,
+          "pitch": 0.9739139955200322,
+          "rotation": 3.141592653589793,
+          "target": "11-deck---port"
+        },
+        {
+          "yaw": 3.1344790049616567,
+          "pitch": 0.11163440269035974,
+          "rotation": 3.141592653589793,
+          "target": "8-wetbar"
+        },
+        {
+          "yaw": 2.7871482083084933,
+          "pitch": 0.1790281045667257,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-interior---bed",
+      "name": "Interior - Bed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9835188839671183,
+        "pitch": 0.15879489854037665,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6977175817779155,
+          "pitch": 0.29195785515799955,
+          "rotation": 3.141592653589793,
+          "target": "3-interior---cabin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-interior---cabin",
+      "name": "Interior - Cabin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.982981431312366,
+        "pitch": 0.23343907545912046,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7645648736154538,
+          "pitch": 0.21547330995344893,
+          "rotation": 3.141592653589793,
+          "target": "2-interior---bed"
+        },
+        {
+          "yaw": 2.9209427738516682,
+          "pitch": 0.2728069538553193,
+          "rotation": 3.141592653589793,
+          "target": "4-interior---head"
+        },
+        {
+          "yaw": -0.12254717987300623,
+          "pitch": 0.051361796085522826,
+          "rotation": 3.141592653589793,
+          "target": "10-midship---starboard"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-interior---head",
+      "name": "Interior - Head",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6059665496842213,
+        "pitch": 0.3555715728245943,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35600814120353164,
+          "pitch": 0.36662406896842015,
+          "rotation": 15.707963267948973,
+          "target": "3-interior---cabin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-aft-deck-",
+      "name": "Aft Deck ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5672561499881965,
+        "pitch": 0.23303891932411425,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.961784242129932,
+          "pitch": 0.5873824325313457,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-deck---center"
+        },
+        {
+          "yaw": -2.165112383673561,
+          "pitch": 1.0635254168865096,
+          "rotation": 3.141592653589793,
+          "target": "1-aft-decklower-step---port"
+        },
+        {
+          "yaw": 1.21280015453487,
+          "pitch": 0.24283272928509447,
+          "rotation": 3.141592653589793,
+          "target": "8-wetbar"
+        },
+        {
+          "yaw": 0.7309258540138934,
+          "pitch": 0.20442086818760075,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-midship---port",
+      "name": "Midship - Port",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.6852461478284955,
+        "pitch": 0.361228435022225,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.040111753994413,
+          "pitch": 0.31254679640560745,
+          "rotation": 3.141592653589793,
+          "target": "5-aft-deck-"
+        },
+        {
+          "yaw": -2.6421373072577765,
+          "pitch": 0.34960159765403986,
+          "rotation": 3.141592653589793,
+          "target": "8-wetbar"
+        },
+        {
+          "yaw": 2.78492503466971,
+          "pitch": 0.34547661010201836,
+          "rotation": 3.141592653589793,
+          "target": "7-helm-station"
+        },
+        {
+          "yaw": 1.1317639869195695,
+          "pitch": 0.26929664659785857,
+          "rotation": 3.141592653589793,
+          "target": "13-bow-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-helm-station",
+      "name": "Helm Station",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6036256299865252,
+        "pitch": 0.38104044245187296,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17252805912437275,
+          "pitch": 0.6273663883439813,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        },
+        {
+          "yaw": -1.343506530337521,
+          "pitch": 0.37267504119623496,
+          "rotation": 3.141592653589793,
+          "target": "8-wetbar"
+        },
+        {
+          "yaw": 3.04142261883798,
+          "pitch": 0.7815666329813844,
+          "rotation": 3.141592653589793,
+          "target": "10-midship---starboard"
+        },
+        {
+          "yaw": -1.5413951052849555,
+          "pitch": 0.16460279183251103,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-deck---center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-wetbar",
+      "name": "Wetbar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.035785092425943,
+        "pitch": 0.39555485818626224,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6648981755029233,
+          "pitch": 0.199474508244883,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        },
+        {
+          "yaw": -1.365781371087305,
+          "pitch": 0.2977542803440443,
+          "rotation": 9.42477796076938,
+          "target": "10-midship---starboard"
+        },
+        {
+          "yaw": 1.2993564914437812,
+          "pitch": 0.3188494015994827,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-deck---center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-top-deck",
+      "name": "Top Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.1509398929305448,
+        "pitch": 0.6670883682170725,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.26657144720593,
+          "pitch": 1.008574423734391,
+          "rotation": 3.141592653589793,
+          "target": "10-midship---starboard"
+        },
+        {
+          "yaw": -2.3273400107515947,
+          "pitch": 0.6236761936597457,
+          "rotation": 3.141592653589793,
+          "target": "12-bow-seat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-midship---starboard",
+      "name": "Midship - Starboard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8442770595087934,
+        "pitch": 0.331792028504573,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.33961997451717,
+          "pitch": 0.5094806064356856,
+          "rotation": 3.141592653589793,
+          "target": "7-helm-station"
+        },
+        {
+          "yaw": 2.8488747856824634,
+          "pitch": 0.8828130656971283,
+          "rotation": 3.141592653589793,
+          "target": "3-interior---cabin"
+        },
+        {
+          "yaw": 2.861043157012703,
+          "pitch": 0.16322706186830693,
+          "rotation": 3.141592653589793,
+          "target": "9-top-deck"
+        },
+        {
+          "yaw": 0.5109435505540105,
+          "pitch": 0.37757659572316093,
+          "rotation": 3.141592653589793,
+          "target": "8-wetbar"
+        },
+        {
+          "yaw": 1.5145109134965988,
+          "pitch": 0.38117496078972124,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        },
+        {
+          "yaw": -0.01484884695024924,
+          "pitch": 0.1785880704094147,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-deck---center"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-deck---port",
+      "name": "Deck - Port",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3931300739377086,
+        "pitch": 0.2838400078333265,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.031748864033484,
+          "pitch": 0.6618488885326048,
+          "rotation": 3.141592653589793,
+          "target": "0-aft-deck---center"
+        },
+        {
+          "yaw": -2.2372190274883117,
+          "pitch": 1.0803505116656709,
+          "rotation": 3.141592653589793,
+          "target": "1-aft-decklower-step---port"
+        },
+        {
+          "yaw": 0.7311668863691381,
+          "pitch": 0.22803950501113945,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        },
+        {
+          "yaw": 1.1431123127062044,
+          "pitch": 0.24803592899152704,
+          "rotation": 3.141592653589793,
+          "target": "8-wetbar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-bow-seat",
+      "name": "Bow Seat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.2860384588584957,
+        "pitch": 0.03139350539525765,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2634328739546614,
+          "pitch": 0.0006642086866861519,
+          "rotation": 3.141592653589793,
+          "target": "9-top-deck"
+        },
+        {
+          "yaw": 3.036780904328946,
+          "pitch": 0.5430250943897228,
+          "rotation": 3.141592653589793,
+          "target": "13-bow-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-bow-",
+      "name": "Bow ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.740242969780363,
+        "pitch": 0.45050661551942284,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2536203953354335,
+          "pitch": 0.14901938033951545,
+          "rotation": 3.141592653589793,
+          "target": "9-top-deck"
+        },
+        {
+          "yaw": 0.970623927244592,
+          "pitch": 0.5833613830100344,
+          "rotation": 3.141592653589793,
+          "target": "12-bow-seat"
+        },
+        {
+          "yaw": 2.225785246103504,
+          "pitch": 0.42740681399978087,
+          "rotation": 3.141592653589793,
+          "target": "6-midship---port"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20231117 Aquila NEBULA-Progress",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

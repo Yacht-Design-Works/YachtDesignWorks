05f394aaab62bab974429c1f27439532caf52523
Tak <tak@yachtdesignworks.com>
2024-07-15 11:14:18 -0400

    Update Broadwater

---
+++ 20240715-broadwater-25-cat (new)/app-files/data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-perspective",
+      "name": "Perspective",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7208682885298838,
+        "pitch": -0.02203567107764215,
+        "fov": 1.3608036366560152
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7954102755379715,
+          "pitch": 0.00577341782854468,
+          "rotation": 3.141592653589793,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 1.5143913433969685,
+          "pitch": -0.008664743597517344,
+          "rotation": 3.141592653589793,
+          "target": "1-helm"
+        },
+        {
+          "yaw": 1.332085937999734,
+          "pitch": 0.04322318248482304,
+          "rotation": 3.141592653589793,
+          "target": "3-stern"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-helm",
+      "name": "Helm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.445060754169246,
+        "pitch": 0.5731469004276342,
+        "fov": 1.4319388366908425
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5710529066442689,
+          "pitch": 0.6425277025592067,
+          "rotation": 3.141592653589793,
+          "target": "3-stern"
+        },
+        {
+          "yaw": 1.5913361056445643,
+          "pitch": 0.21732913142521681,
+          "rotation": 3.141592653589793,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 2.9291600952017474,
+          "pitch": 0.3017208316190363,
+          "rotation": 3.141592653589793,
+          "target": "0-perspective"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bow",
+      "name": "Bow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5201806560640083,
+        "pitch": 0.7163143596866401,
+        "fov": 1.4319388366908425
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5839061910721357,
+          "pitch": 0.2866180583760318,
+          "rotation": 3.141592653589793,
+          "target": "1-helm"
+        },
+        {
+          "yaw": 1.5801721801354889,
+          "pitch": 0.17555686549446747,
+          "rotation": 3.141592653589793,
+          "target": "3-stern"
+        },
+        {
+          "yaw": 0.39432303934460755,
+          "pitch": 0.2258226659561391,
+          "rotation": 3.141592653589793,
+          "target": "0-perspective"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-stern",
+      "name": "Stern",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.2557930424155197,
+        "pitch": 0.46638153965455587,
+        "fov": 1.4319388366908425
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5659701062533848,
+          "pitch": 0.2643170048363004,
+          "rotation": 3.141592653589793,
+          "target": "1-helm"
+        },
+        {
+          "yaw": 1.5679018139479348,
+          "pitch": 0.023032825772904886,
+          "rotation": 3.141592653589793,
+          "target": "2-bow"
+        },
+        {
+          "yaw": 2.7127916643987,
+          "pitch": 0.23017747155054025,
+          "rotation": 3.141592653589793,
+          "target": "0-perspective"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "20240715 Broadwater 25 CAT ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

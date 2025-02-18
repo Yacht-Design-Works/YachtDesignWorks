77485886ab6f4c52f3f8165e0b510c7fdce29717
Tak <tak@yachtdesignworks.com>
2025-02-18 10:30:23 -0500

    queen customs 53 cc

---
+++ queen-custom-53-cc/app-files/data.js
@@ -0,0 +1,517 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-profile",
+      "name": "Profile",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5450152269779815,
+        "pitch": -0.17248346555969896,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7070004371202874,
+          "pitch": -0.0685930548567768,
+          "rotation": 3.141592653589793,
+          "target": "5-helm"
+        },
+        {
+          "yaw": 2.3236651272603925,
+          "pitch": -0.0739683385681893,
+          "rotation": 3.141592653589793,
+          "target": "1-bow"
+        },
+        {
+          "yaw": 1.3640530854232118,
+          "pitch": -0.3734644893284589,
+          "rotation": 3.141592653589793,
+          "target": "9-tower"
+        },
+        {
+          "yaw": 1.0593707388415794,
+          "pitch": 0.016181883574851952,
+          "rotation": 3.141592653589793,
+          "target": "7-aft-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bow",
+      "name": "Bow",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6017666987551795,
+        "pitch": 0.3825487899805289,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.212815291047784,
+          "pitch": 0.28281214870126803,
+          "rotation": 3.141592653589793,
+          "target": "2-stbd-forward"
+        },
+        {
+          "yaw": 1.628012015613372,
+          "pitch": -0.29415642666194763,
+          "rotation": 3.141592653589793,
+          "target": "9-tower"
+        },
+        {
+          "yaw": 0.456172943687001,
+          "pitch": 0.18682888851941826,
+          "rotation": 3.141592653589793,
+          "target": "0-profile"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-stbd-forward",
+      "name": "STBD Forward",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.568253283018251,
+        "pitch": 0.37014961189955997,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.401145280905725,
+          "pitch": 0.6629989320652108,
+          "rotation": 3.141592653589793,
+          "target": "3-port-cabin"
+        },
+        {
+          "yaw": 1.0879142851739445,
+          "pitch": 0.18537704703080493,
+          "rotation": 3.141592653589793,
+          "target": "7-aft-deck"
+        },
+        {
+          "yaw": -2.5370478186941767,
+          "pitch": 0.1890940891430013,
+          "rotation": 3.141592653589793,
+          "target": "1-bow"
+        },
+        {
+          "yaw": -0.4144668987540392,
+          "pitch": 0.2198515193062729,
+          "rotation": 3.141592653589793,
+          "target": "0-profile"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-port-cabin",
+      "name": "PORT Cabin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.470645700647447,
+        "pitch": 0.46018729424163496,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1357894242155204,
+          "pitch": 0.14443814247900022,
+          "rotation": 3.141592653589793,
+          "target": "2-stbd-forward"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-stbd-head",
+      "name": "STBD Head",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8964233884972117,
+        "pitch": 0.5381652933847434,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.19524043353115772,
+          "pitch": 0.28899675954660964,
+          "rotation": 3.141592653589793,
+          "target": "6-wet-bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-helm",
+      "name": "Helm",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6119975436381022,
+        "pitch": 0.047668389915184406,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6119975436381022,
+          "pitch": 0.047668389915184406,
+          "rotation": 3.141592653589793,
+          "target": "1-bow"
+        },
+        {
+          "yaw": -3.003118155725801,
+          "pitch": 0.10583543568450615,
+          "rotation": 3.141592653589793,
+          "target": "0-profile"
+        },
+        {
+          "yaw": -0.8601515400044057,
+          "pitch": 0.219089862759418,
+          "rotation": 3.141592653589793,
+          "target": "6-wet-bar"
+        },
+        {
+          "yaw": -1.560643997504318,
+          "pitch": 0.17364695325082558,
+          "rotation": 3.141592653589793,
+          "target": "8-rocket-launchers"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-wet-bar",
+      "name": "Wet Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.050518688373776,
+        "pitch": 0.5154760461134131,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3494509232608518,
+          "pitch": 0.040568437213270414,
+          "rotation": 3.141592653589793,
+          "target": "5-helm"
+        },
+        {
+          "yaw": 2.263140898498456,
+          "pitch": 0.3035238946635239,
+          "rotation": 3.141592653589793,
+          "target": "8-rocket-launchers"
+        },
+        {
+          "yaw": -0.18390528915148963,
+          "pitch": 0.13264737434841756,
+          "rotation": 3.141592653589793,
+          "target": "4-stbd-head"
+        },
+        {
+          "yaw": -0.1938654676909053,
+          "pitch": 0.016954311055318527,
+          "rotation": 3.141592653589793,
+          "target": "1-bow"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-aft-deck",
+      "name": "Aft Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8359101716821495,
+        "pitch": -0.07421337617298462,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8353389365919046,
+          "pitch": 0.15055331883190703,
+          "rotation": 3.141592653589793,
+          "target": "0-profile"
+        },
+        {
+          "yaw": 1.6563698661283297,
+          "pitch": 0.02317474545515985,
+          "rotation": 3.141592653589793,
+          "target": "6-wet-bar"
+        },
+        {
+          "yaw": 1.6799486364846095,
+          "pitch": 0.2643431826831204,
+          "rotation": 3.141592653589793,
+          "target": "8-rocket-launchers"
+        },
+        {
+          "yaw": 2.0113906638626924,
+          "pitch": -0.05647584529837424,
+          "rotation": 3.141592653589793,
+          "target": "5-helm"
+        },
+        {
+          "yaw": 1.7810667708373797,
+          "pitch": -0.8106028118781978,
+          "rotation": 0,
+          "target": "9-tower"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-rocket-launchers",
+      "name": "Rocket Launchers",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6056585391884068,
+        "pitch": 0.4613939153386646,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.003241171579944,
+          "pitch": 0.45526519122587317,
+          "rotation": 3.141592653589793,
+          "target": "7-aft-deck"
+        },
+        {
+          "yaw": -1.541875593092449,
+          "pitch": -0.04539513982934906,
+          "rotation": 3.141592653589793,
+          "target": "5-helm"
+        },
+        {
+          "yaw": -2.12191973908004,
+          "pitch": 0.1048720898985529,
+          "rotation": 3.141592653589793,
+          "target": "6-wet-bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-tower",
+      "name": "Tower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6203713917693925,
+        "pitch": 0.4656491421657307,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5845328656644906,
+          "pitch": 0.4411039012850413,
+          "rotation": 3.141592653589793,
+          "target": "1-bow"
+        },
+        {
+          "yaw": 3.0498151474132555,
+          "pitch": 0.5032355783071925,
+          "rotation": 3.141592653589793,
+          "target": "0-profile"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Queen Custom 53 CC",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
